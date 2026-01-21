aeb444c919f2dd19a53b8976ac1ad724037bb69f
Takshsri <aymar131221@gmail.com>
2026-01-21 13:13:17 +0530

    Add

--- eventz-frontend/src/utils/axios.ts
+++ eventz-frontend/src/utils/axios.ts
@@ -2,9 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
 
 api.interceptors.request.use(
   (config) => {
