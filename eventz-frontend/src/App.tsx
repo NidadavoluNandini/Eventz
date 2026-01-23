635b7fb918d73ce398172394bdb84e909a4db7dd
Takshsri <aymar131221@gmail.com>
2026-01-23 08:25:32 +0530

    Add

--- eventz-frontend/src/App.tsx
+++ eventz-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-/* PUBLIC PAGES */
+/* PUBLIC */
 import EventsList from "./pages/public/EventsList";
 import EventDetails from "./pages/public/EventDetails";
 import RegisterEvent from "./pages/public/RegisterEvent";
@@ -8,16 +8,16 @@ import VerifyOtp from "./pages/public/VerifyOtp";
 import TicketSuccess from "./pages/public/TicketSuccess";
 import Payment from "./pages/public/Payment";
 import PaymentCancelled from "./pages/public/PaymentCancelled";
+import ResumeRegistration from "./pages/public/ResumeRegistration";
+import RegistrationExpired from "./pages/public/RegistrationExpired";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
 
-/* ORGANIZER AUTH (PUBLIC) */
+/* ORGANIZER */
 import OrganizerLogin from "./pages/organizer/Login";
 import Register from "./pages/organizer/Register";
 import OrganizerForgotPassword from "./pages/organizer/OrganizerForgotPassword";
 import OrganizerResetPassword from "./pages/organizer/OrganizerResetPassword";
-
-/* ORGANIZER PROTECTED */
 import Dashboard from "./pages/organizer/Dashboard";
 import Events from "./pages/organizer/Events";
 import CreateEvent from "./pages/organizer/CreateEvent";
@@ -26,28 +26,29 @@ import OrganizerLayout from "./pages/organizer/OrganizerLayout";
 
 /* PROTECTION */
 import ProtectedRoute from "./components/ProtectedRoute";
-import ResumeRegistration from "./pages/public/ResumeRegistration";
-import RegistrationExpired from "./pages/public/RegistrationExpired";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         {/* ========== PUBLIC ROUTES ========== */}
         <Route path="/" element={<EventsList />} />
         <Route path="/events/:id" element={<EventDetails />} />
         <Route path="/events/:id/register" element={<RegisterEvent />} />
-        <Route path="/verify-otp" element={<VerifyOtp />} />
+
+        {/* 🔥 RESUME FLOW */}
+        <Route path="/resume/:registrationId" element={<ResumeRegistration />} />
+        <Route path="/verify-otp/:registrationId" element={<VerifyOtp />} />
+        <Route path="/payment/:registrationId" element={<Payment />} />
         <Route path="/ticket-success/:id" element={<TicketSuccess />} />
-        <Route path="/payment/:id" element={<Payment />} />
-        <Route
-          path="/payment-cancelled/:id"
-          element={<PaymentCancelled />}
-        />
+        <Route path="/payment-cancelled/:id" element={<PaymentCancelled />} />
+        <Route path="/registration-expired" element={<RegistrationExpired />} />
+
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
 
-        {/* ========== ORGANIZER AUTH (PUBLIC) ========== */}
+        {/* ========== ORGANIZER AUTH ========== */}
         <Route path="/organizer/login" element={<OrganizerLogin />} />
         <Route path="/organizer/register" element={<Register />} />
         <Route
@@ -59,7 +60,7 @@ export default function App() {
           element={<OrganizerResetPassword />}
         />
 
-        {/* ========== ORGANIZER PROTECTED AREA ========== */}
+        {/* ========== ORGANIZER PROTECTED ========== */}
         <Route
           path="/organizer"
           element={
@@ -72,12 +73,8 @@ export default function App() {
           <Route path="events" element={<Events />} />
           <Route path="events/create" element={<CreateEvent />} />
           <Route path="users" element={<UserManagement />} />
-          <Route path="/resume/:registrationId" element={<ResumeRegistration />} />
-          <Route path="/verify-otp/:registrationId" element={<VerifyOtp />} />
-          <Route path="/payment/:registrationId" element={<Payment />} />
-          <Route path="/registration-expired" element={<RegistrationExpired />} />
-
         </Route>
+
       </Routes>
     </BrowserRouter>
   );
