7ebcdf05f70504bc6733b0e6cc20f9cec679830b
Takshsri <aymar131221@gmail.com>
2026-01-24 13:24:55 +0530

    Add

--- backend/src/organizers/organizer.module.ts
+++ backend/src/organizers/organizer.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Organizer, OrganizerSchema } from './schemas/organizer.schema';
 import { OrganizersService } from './organizer.service';
+import { OrganizersController } from './organizers.controller';
 
 @Module({
   imports: [
@@ -9,6 +10,8 @@ import { OrganizersService } from './organizer.service';
       { name: Organizer.name, schema: OrganizerSchema },
     ]),
   ],
+    controllers: [OrganizersController], // ✅ VERY IMPORTANT
+
   providers: [OrganizersService],
   exports: [OrganizersService],
 })
